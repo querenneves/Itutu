225dc53e7c4ce1a055746280ac2354235787b09f
querenneves <querensantos1@hotmail.com>
2024-02-17 12:33:16 -0300

    Projeto contra a dengue

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('joinBtn').addEventListener('click', function() {
+    window.location.href = 'https://chat.whatsapp.com/EI1kmZ9avDUD6ox7Iry6Fm'; // Substitua 'https://linkParaOGrupo' pelo link real do seu grupo
+});
