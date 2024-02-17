163c5d14668bc3d52ec140bc5ccd9864b3c0db3d
Queren Neves <80592870+querenneves@users.noreply.github.com>
2024-02-17 18:41:15 -0300

    mosquito voando

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.getElementById('joinBtn').addEventListener('click', function() {
     window.location.href = 'https://chat.whatsapp.com/EI1kmZ9avDUD6ox7Iry6Fm'; // Substitua 'https://linkParaOGrupo' pelo link real do seu grupo
 });
+
+const mosquito = document.getElementById('mosquito');
+
+function voar() {
+    const x = Math.random() * (window.innerWidth - mosquito.offsetWidth);
+    const y = Math.random() * (window.innerHeight - mosquito.offsetHeight);
+    mosquito.style.transform = `translate(${x}px, ${y}px)`;
+}
+
+setInterval(voar, 3000); 
+
+
