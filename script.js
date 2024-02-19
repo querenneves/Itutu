d7c286e77928756971708e4dc144130d64b8d9c8
Queren Neves <80592870+querenneves@users.noreply.github.com>
2024-02-18 21:10:50 -0300

    ajuste

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('joinBtn').addEventListener('click', function() {
-    window.location.href = 'https://chat.whatsapp.com/EI1kmZ9avDUD6ox7Iry6Fm'; // Substitua 'https://linkParaOGrupo' pelo link real do seu grupo
+    window.location.href = 'https://chat.whatsapp.com/EI1kmZ9avDUD6ox7Iry6Fm'; 
 });
 
 const mosquito = document.getElementById('mosquito');
